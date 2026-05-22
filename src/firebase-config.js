bccdcb35ab0da52aab2cd9a06c64f9afd21a363e
EnrLobo <enriquealfenas@gmail.com>
2026-05-22 15:07:58 -0300

    Firebase in

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,11 @@
-export const useFirebase = false;
+export const useFirebase = true;
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyD36JUydtQ3hWAE0X99mNfUQQ7cixmUyc8",
+  authDomain: "bolaowc-1fd6a.firebaseapp.com",
+  projectId: "bolaowc-1fd6a",
+  storageBucket: "bolaowc-1fd6a.firebasestorage.app",
+  messagingSenderId: "561119747891",
+  appId: "1:561119747891:web:638d6e526af9c8c3d62846",
+  measurementId: "G-CKRD730Y75",
 };
